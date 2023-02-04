9c51485edb566e2df7187cc8612dc076edf3d091
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-03 21:17:19 -0300

    iniciando a logica para o contador

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ const buttonSet = document.querySelector(".set");
 const buttonSoundOn = document.querySelector(".sound-on");
 const buttonSoundOff = document.querySelector(".sound-off");
 
+let minutes;
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
+
+function countDown() {
+  setTimeout(() => {
+    let seconds = Number(secondsDisplay.textContent);
+
+    if (seconds <= 0) {
+      seconds = 60;
+    }
+
+    secondsDisplay.textContent = seconds - 1;
+    countDown();
+  }, 1000);
+}
+
 buttonPlay.addEventListener("click", () => {
   buttonPlay.classList.add("hide");
   buttonPause.classList.remove("hide");
   buttonSet.classList.add("hide");
   buttonStop.classList.remove("hide");
+
+  countDown();
 });
 
 buttonPause.addEventListener("click", () => {
@@ -32,4 +51,9 @@ buttonSoundOff.addEventListener("click", () => {
 buttonSoundOn.addEventListener("click", () => {
   buttonSoundOn.classList.add("hide");
   buttonSoundOff.classList.remove("hide");
+});
+
+buttonSet.addEventListener("click", () => {
+  minutes = prompt("Quantos minutos?");
+  minutesDisplay.textContent = minutes;
 });
