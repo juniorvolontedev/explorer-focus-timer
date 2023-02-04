b90d23903f5a5471af6c31dd03a745d99c8992bc
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-04 07:13:41 -0300

    principios de clean code e programacao declarativa

--- script.js
+++ script.js
@@ -8,29 +8,36 @@ let minutes;
 const minutesDisplay = document.querySelector(".minutes");
 const secondsDisplay = document.querySelector(".seconds");
 
+function resetControls() {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  buttonSet.classList.remove("hide");
+  buttonStop.classList.add("hide");
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  secondsDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
 function countdown() {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent);
     let minutes = Number(minutesDisplay.textContent);
 
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+    updateTimerDisplay(minutes, 0);
 
     if (minutes <= 0) {
-      buttonPlay.classList.remove("hide");
-      buttonPause.classList.add("hide");
-      buttonSet.classList.remove("hide");
-      buttonStop.classList.add("hide");
-
+      resetControls();
       return;
     }
 
     if (seconds <= 0) {
       seconds = 2;
-
-      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0");
+      --minutes;
     }
 
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+    updateTimerDisplay(minutes, String(seconds - 1));
 
     countdown();
   }, 1000);
@@ -51,10 +58,7 @@ buttonPause.addEventListener("click", function () {
 });
 
 buttonStop.addEventListener("click", function () {
-  buttonPlay.classList.remove("hide");
-  buttonPause.classList.add("hide");
-  buttonSet.classList.remove("hide");
-  buttonStop.classList.add("hide");
+  resetControls();
 });
 
 buttonSoundOff.addEventListener("click", function () {
@@ -69,5 +73,5 @@ buttonSoundOn.addEventListener("click", function () {
 
 buttonSet.addEventListener("click", function () {
   minutes = prompt("Quantos minutos?");
-  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  updateTimerDisplay(minutes, 0);
 });
