44c15183bdc3fff90b0823ff1ed027aa79257f77
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-03 21:06:08 -0300

    refatorando para as novas funcionalidades

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let play = document.querySelector(".play");
-let pause = document.querySelector(".pause");
+const buttonPlay = document.querySelector(".play");
+const buttonPause = document.querySelector(".pause");
+const buttonStop = document.querySelector(".stop");
+const buttonSet = document.querySelector(".set");
+const buttonSoundOn = document.querySelector(".sound-on");
+const buttonSoundOff = document.querySelector(".sound-off");
 
-play.addEventListener("click", () => {
-  play.classList.add("hide");
-  pause.classList.remove("hide");
+buttonPlay.addEventListener("click", () => {
+  buttonPlay.classList.add("hide");
+  buttonPause.classList.remove("hide");
+  buttonSet.classList.add("hide");
+  buttonStop.classList.remove("hide");
 });
 
-pause.addEventListener("click", () => {
-  play.classList.remove("hide");
-  pause.classList.add("hide");
+buttonPause.addEventListener("click", () => {
+  buttonPause.classList.add("hide");
+  buttonPlay.classList.remove("hide");
+});
+
+buttonStop.addEventListener("click", () => {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  buttonSet.classList.remove("hide");
+  buttonStop.classList.add("hide");
+});
+
+buttonSoundOff.addEventListener("click", () => {
+  buttonSoundOn.classList.remove("hide");
+  buttonSoundOff.classList.add("hide");
+});
+
+buttonSoundOn.addEventListener("click", () => {
+  buttonSoundOn.classList.add("hide");
+  buttonSoundOff.classList.remove("hide");
 });
